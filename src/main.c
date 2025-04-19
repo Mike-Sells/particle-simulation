ca5f93b66dad3e5abd2833f90873b549f808537f
mike-sells <sells@lancaster.ac.uk>
2025-04-19 15:53:53 +0000

    defined the structure for the particle type

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+* Structure: Particle
+*   stores the values for each particle 
+*
+* float arrays will hold directional x, y information of the two particles
+* float radius determins the size of the shown particle
+*/
+
+typedef struct Particle {
+    float[2] displacement;
+    float[2] velocity;
+    float radius;
+} p;
+
+int main(void) {
+
+}
