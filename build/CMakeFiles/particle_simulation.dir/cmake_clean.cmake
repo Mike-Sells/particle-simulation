b40d256655bed50a1a13d3ee7634c3632977a3f5
mike-sells <sells@lancaster.ac.uk>
2025-04-19 15:45:35 +0000

    project init

---
+++ build/CMakeFiles/particle_simulation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/particle_simulation.dir/link.d"
+  "CMakeFiles/particle_simulation.dir/src/main.c.o"
+  "CMakeFiles/particle_simulation.dir/src/main.c.o.d"
+  "particle_simulation"
+  "particle_simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/particle_simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/particle_simulation.dir/src/main.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/particle_simulation.dir/src/main.c.o: \
+ /home/mike/Documents/particle-simulation/src/main.c \
+ /usr/include/stdc-predef.h
